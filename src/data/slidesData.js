759b5acab5b8322cd374fc7f9a00b02289d914ed
Marcio <marciojsjunior.mj@gmail.com>
2025-10-27 18:48:25 -0300

    dfhs

--- src/data/slidesData.js
+++ src/data/slidesData.js
@@ -1,60 +1,66 @@
 // src/data/slidesData.js
 
+import imagemCarrossel1 from '../assets/imagemCarrossel1.webp';
+import imagemCarrossel2 from '../assets/imagemCarrossel2.webp';
+import imagemCarrossel3 from '../assets/imagemCarrossel3.webp';
+import imagemCarrossel4 from '../assets/imagemCarrossel4.webp';
+import imagemCarrossel5 from '../assets/imagemCarrossel5.webp';
+import imagemCarrossel6 from '../assets/imagemCarrossel6.webp';
+import imagemCarrossel7 from '../assets/imagemCarrossel7.webp';
+import imagemCarrossel8 from '../assets/imagemCarrossel8.webp';
+import imagemCarrossel9 from '../assets/imagemCarrossel9.webp';
+
 export const slidesData = [
   {
-    image: 'src/assets/imagemCarrossel1.webp',
+    image: imagemCarrossel1,
     location: 'Vale Encantado',
     title: 'Amanhecer nas Montanhas',
     author: 'Por Ricardo Gomes',
   },
   {
-    image: 'src/assets/imagemCarrossel2.webp',
+    image: imagemCarrossel2,
     location: 'Erzurum, Türkiye',
     title: 'Cerejas Vermelhas',
     author: 'Büsra Aksoy',
   },
   {
-    // NOVO LINK - Imagem 2
-    image: 'src/assets/imagemCarrossel3.webp',
+    image: imagemCarrossel3,
     location: 'São Paulo',
     title: 'Noite Urbana',
     author: 'Por Ana Clara',
   },
   {
-    image: 'src/assets/imagemCarrossel4.webp',
+    image: imagemCarrossel4,
     location: 'Dōtonbori, Chūō-ku, Osaka, Prefettura di Osaka, Giappone',
     title: 'Luzes da cidade',
     author: 'Sir. Simo',
   },
   {
-    // NOVO LINK - Imagem 4
-    image: 'src/assets/imagemCarrossel5.webp',
+    image: imagemCarrossel5,
     location: 'Costa Litoral',
     title: 'Ondas do Atlântico',
     author: 'Por Sofia Alves',
   },
   {
-    image: 'src/assets/imagemCarrossel6.webp',
+    image: imagemCarrossel6,
     location: 'Serra da Estrela',
     title: 'Inverno Silencioso',
     author: 'Por Marco Aurélio',
   },
   {
-    // NOVO LINK - Imagem 6
-    image: 'src/assets/imagemCarrossel7.webp',
+    image: imagemCarrossel7,
     location: 'Estúdio Criativo',
     title: 'Retrato de um Olhar',
     author: 'Por Juliana Paiva',
   },
   {
-    image: 'src/assets/imagemCarrossel8.webp',
+    image: imagemCarrossel8,
     location: 'Centro Urbano',
     title: 'Arquitetura Moderna',
     author: 'Por Tiago Ferreira',
   },
   {
-    // NOVO LINK - Imagem 8
-    image: 'src/assets/imagemCarrossel9.webp',
+    image: imagemCarrossel9,
     location: 'Parque Nacional',
     title: 'Trilhas e Aventuras',
   }
